0ee1e7f7a8e653ab6dc19f59d8aa111c1351bbdb
jayjay1509 <jayson.malet@me.com>
2023-09-13 11:14:14 +0200

    test 3

--- c++/projet 06.09.20223/calculatrice 2/calculatrice 2.cpp
+++ c++/projet 06.09.20223/calculatrice 2/calculatrice 2.cpp
@@ -10,37 +10,42 @@ int main()
 
 	std::cout << "nombre a :\t";
 	std::cin >> a;
-	
+
+	std::cout << "operation :\t";
+	std::cin >> c;	
+
 	std::cout << "nombre b :\t";
 	std::cin >> b;
 
-	std::cout << "operation :\t";
-	std::cin >> c;
+
 
 	if (c == '+')
 	{
-		std::cout << "somme =\t" << add(a, b);
+		std::cout << "somme =\t\t" << add(a, b);
 	}
 	else if (c == '-')
 	{
-		std::cout << "difference =\t" <<substract(a, b);
+		std::cout << "difference =\t\t" <<substract(a, b);
 	}
 	else if (c == '*')
 	{
-		std::cout << "produit =\t" << multiply(a, b);
+		std::cout << "produit =\t\t" << multiply(a, b);
 	}
 	else if (c == '/')
 	{
 		if (a == 0)
 		{
 			std::cout << "operation non valide ";
+			return EXIT_FAILURE;
 		}
 		else 
-		std::cout << "quotient =\t" << divide(a, b);
+		std::cout << "quotient =\t\t" << divide(a, b);
 	}
 	else
 	{
 		std::cout << "operation non valide ";
 	}
+	return EXIT_SUCCESS;
 }
+
 
