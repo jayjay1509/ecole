1cc61b5aaaf4330ce220fd6ad616c3bde1c87fc7
jayjay1509 <jayson.malet@gmail.com>
2023-09-19 14:00:25 +0200

    finition de la chasse au tresore

--- c++/projet 06.09.20223/chasse au tresor/chasse au tresor.cpp
+++ c++/projet 06.09.20223/chasse au tresor/chasse au tresor.cpp
@@ -1,84 +1,136 @@
-
 #include <iostream>
-#define width 8
-#define height 16
+#include <cstdlib>
+#include <ctime>
+#define width 4
+#define height 4
+
+int cases[width * height];
+
+//c'est pour randomiser un nombre de l'array qui fais 4 par 4 pour savoir se se trouvera le tresor 
+int tresor() {
+
+   
+    int randomValue = rand() % (width * height + 1);
+    return randomValue;
+}
+// c'est la suite. la on  utilise le chiffre que l'on a randomiser juste avant pour pour le modifier dans array et on le fais deux fois avec la boucle for  
+void randomarray() {
+    for (int i = 0; i < 2; ++i) {
+        cases[tresor()] = 1;
+    }
+}
+
+//ici c'est pour dessiner le plateau j'y ai rajoute else if avec un pour pouvoir controle ou sont les tresor il peu etre ammeliore mais je perd c'est fonctionaliter 
 void drawplayfield() {
-	int cases[width * height]{
-			0,0,0,0,0,0,0,0,
-			0,0,0,0,0,0,0,0,
-			0,0,0,0,0,0,0,0,
-			0,0,0,0,0,0,0,0,
-			0,0,0,0,0,0,0,0,
-			0,0,0,0,0,0,0,0,
-			0,1,0,0,0,0,0,0,
-			0,0,0,0,0,0,0,0,
-			0,0,0,0,0,0,0,0,
-			0,0,0,0,0,0,0,0,
-			0,0,0,0,0,0,0,0,
-			0,0,0,0,0,0,0,0,
-			0,0,0,0,0,0,0,0,
-			0,0,0,0,0,0,0,0,
-			0,0,0,0,0,0,0,0,
-			0,0,0,0,0,0,0,0,
-	};
-	int a = 0;
-	for (int row = 0; row < height; row++) {
-		for (int column = 0; column < width; column++) {
-			int case_state = cases[a++];
-			// int index = row * 8 + clumn
-			if (case_state == 0) {
-				std::cout << '-' << '\t';
-			}
-			else
-			{
-				std::cout << 'x' << '\t';
-			}
-		}
-		std::cout << '\n';
-	}
+    int a = 0;
+    std::cout << "pour jouer veuille rentre en premier la ligne puis la cologne\n\n";
+    std::cout << "\t[1]\t[2]\t[3]\t[4]\n";
+    for (int row = 0; row < height; row++) {
+        std::cout << "[" << row+1 << "]\t";
+        for (int column = 0; column < width; column++) {
+            int case_state = cases[a++];
+            if (case_state == 0) {
+                std::cout << " -" << '\t';
+            }
+            else if (case_state == 1)
+            {
+                std::cout << " x" << '\t';
+            }
+            else if (case_state == 3)
+            {
+                std::cout << " $" << '\t';
+            }
+            else {
+                std::cout << " O" << '\t';
+            }
+        }
+        std::cout << '\n';
+    }
 }
 
-void input() 
-{
-	int x;
-	int y;
-	std::cout << "coordonnee de recherche ?\n";
+//ici c'est les imput pour controlere la ligne et la colonge j'y ai ajjoute des commande que jai trouve sur internet mais je lais ai pas tres bien compris si je peu avoir un explication ?
+int input() {
+    int x;
+    int y;
+    bool validInput = false;
 
-	std::cout << "x :";
-	std::cin >> x;
-	std::cout << "y :";
-	std::cin >> y;
+    do {
+        std::cout << "\nCoordonnees de recherche ?\n";
 
-	//int result;
-	
-}
-// il faut deux tappbleua 
+        std::cout << "x : ";
+        if (std::cin >> x) {
+            std::cout << "y : ";
+            if (std::cin >> y) {
+                validInput = true; 
+            }
+            else {
+                std::cout << "Veuillez entrer un nombre valide pour 'y'.\n";
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            }
+        }
+        else {
+            std::cout << "Veuillez entrer un nombre valide pour 'x'.\n";
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        }
+        //. Si l'une des saisies échoue (par exemple, si l'utilisateur entre une lettre au lieu d'un nombre),
+        //  nous affichons un message d'erreur, effaçons le flux d'entrée et ignorons les caractères restants jusqu'à la fin de la ligne (std::cin.ignore(...)).
+    } while (!validInput);
 
-int main()
-{
-	bool tresurefound = false;
-	
-
-	do
-	{
-		int idxSearch = input();
-
-		if (cases[idxSearch] == 0)
-		{
-			std:: << "pas de tresor\n";
-		}
-		else
-		{
-			std:: << " tresor\n";
-		}
-		input();
-
-		drawplayfield();
-
-	} while (tresurefound == false);
-	
-	
-
-		return EXIT_SUCCESS;
+    int index = ((x - 1) * 4 + y) - 1;
+    return index;
 }
 
+// ici tout se fais au debut sa appelle et genere les trsore puis sa dessine le plateau puis sa demande de metre la ligne et collogne et si utilisateur mets un nombre plus grand sa lui dit pas posiblée 
+
+int main() {
+    bool treasurefound = false;
+    std::srand(std::time(nullptr));
+    randomarray();
+    drawplayfield();
+
+    do {
+        int idxSearch = input();
+ 
+        if (idxSearch < 16)
+        {
+            if (cases[idxSearch] == 0) {
+                int pelle = 2;
+                cases[idxSearch] = pelle;
+                std::cout << "\npas de tresor\n\n";
+
+            }
+            else if (cases[idxSearch] == 2) {
+                std::cout << "tu ne peu pas creuser ou tu la deja fais\n";
+            }
+            
+            else {
+                int pelletrouvetresore = 3;
+                cases[idxSearch] = pelletrouvetresore;
+                std::cout << "\ntresor\n";
+                treasurefound = true;
+            }
+        }
+        else
+        {
+            std::cout << "c'est plus posible\n";
+        }
+        drawplayfield();
+
+    } while (!treasurefound);
+    std::cout << "\n\n\n\n\n\t\t\t\t\t\tfelicitation vous avez trouve le tresor\n\n\n";
+
+    printf("    *             _____          ..-~---------------- ~-..-~\n");
+    printf("   * *           |     |   \~~~\.'                    `./~~~/\n");
+    printf("  *   *         ---------   \__/                        \__/\n");
+    printf(" *     *       .'  O    \     /               /       \  '\n");
+    printf("*       *     (_____,    `._.'               |         }  \/~~~/\n");
+    printf("*********      `----.          /       }     |        /    \__/\n");
+    printf("*       *            `-.      |       /      |       /      `. ,~~|\n");
+    printf("*       *                ~-.__|      /_ - ~ ^|      /- _      `..-'\n");
+    printf("*       *                     |     /        |     /     ~-.     `-. _  _  _\n");
+    printf("*********                     |_____|        |_____|         ~ - . _ _ _ _ _>\n");
+
+    return 0;
+}
